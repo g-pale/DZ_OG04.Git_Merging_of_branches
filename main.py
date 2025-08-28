d0444dd24e3f5f9f005d2f91c72e0a4c10dd9e38
g-pale <saburkin.s@outlook.com>
2025-08-28 14:23:35 +0300

    Update User1 main

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Программа для нахождения четных чисел в диапазоне')
+
+numbers = []
+
+list_begin = int(input('Введите число начала диапазона: - '))
+list_end = int(input('Введите число конца диапазона: - '))
+
+for i in range(list_begin, list_end + 1):
+  numbers.append(i)
+print(f'Диапазон Ваших чисел {numbers}')
+
+for element in numbers:
+    if element % 2 == 0:
+        print(f'Число {element} четное')
